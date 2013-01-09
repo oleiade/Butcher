e157e92504dcc78d75f35831efd63aea0279d057
Oleiade <tcrevon@gmail.com>
2013-01-09 22:17:58 +0100

    Add : clean reader/writer + clean cmdline args management

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "fmt"
+  "log"
   "flag"
   "os"
   "io"
@@ -11,28 +11,52 @@ import (
 // /*
 // ** Command line arguments
 // */
-var input = flag.String("input", "", "Input file")
-var output = flag.String("output", "", "Output dir")
-var sample = flag.Int("sample", 10, "How many lines by file?")
+var input string
+var output string
+var sample string
+
+func init() {
+  const (
+    defaultInput = "Input file"
+    defaultOutput = "Ouput directory"
+    defaultSample = "Line count per output file"
+    usage = "butcher -input bigfatfile -output dir -sample linecountpersubfile"
+  )
+
+  flag.StringVar(&input, "input", defaultInput, usage)
+  flag.StringVar(&input, "i", defaultInput, usage + " (shorthand)")
+
+  flag.StringVar(&output, "output", defaultOutput, usage)
+  flag.StringVar(&output, "o", defaultOutput, usage + " (shorthand)")
+
+  flag.StringVar(&sample, "sample", defaultSample, usage)
+  flag.StringVar(&sample, "s", defaultSample, usage + " (shorthand)")
+}
 
 func check(e error) {
     if e != nil {
-        panic(e)
+        log.Fatal(e)
     }
 }
 
 func main() {
     flag.Parse()
-    file, err := os.Open(*input)
-    check(err)
 
-    reader := bufio.NewReader(file)
+    input_file, err := os.Open(input)
+    if err != nil { log.Fatal(err) }
+    defer input_file.Close()
+
+    output_file, err := os.Create(output)
+    if err != nil { log.Fatal(err) }
+    defer output_file.Close()
+
+    reader := bufio.NewReader(input_file)
+    writer := bufio.NewWriter(output_file)
 
     for {
         line, _, err := reader.ReadLine()
-        if err == io.EOF {
-            break
-        }
-        fmt.Println(string(line))
+        if err == io.EOF { break }
+        writer.WriteString(string(line))
+        if err != nil { log.Fatal(err) }
     }
 }
