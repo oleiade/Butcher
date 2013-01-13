4b8c9c2b691e8a173ef51500bb07c46740d95ba3
Oleiade <tcrevon@gmail.com>
2013-01-13 23:44:33 +0100

    Update: succesfuly creates output dir, and writes output part files

--- files.go
+++ files.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-    "os"
+    "strconv"
     "bufio"
+    "os"
 )
 
-func genReader(filepath string) (*bufio.Reader, *os.File, error) {
+// exists returns whether the given file or directory exists or not
+// Source: http://stackoverflow.com/a/10510783
+func exists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil { return true, nil }
+    if os.IsNotExist(err) { return false, nil }
+
+    return false, err
+}
+
+// genReader helper function acquires a bufio.Reader instance
+// pointer to a O_RONLY mode openend file.
+// You can use it to, for example, read a file line by line.
+// It takes in parameter the path to the file you'd wanna
+// acquire a Reader for, and returns a bufio.Reader instance pointer,
+// a pointer to the opened os.File descriptor and error.
+func GenReader(filepath string) (*bufio.Reader, *os.File, error) {
     input_file, err := os.Open(filepath)
     if err != nil { return nil, nil, err }
 
@@ -14,11 +31,28 @@ func genReader(filepath string) (*bufio.Reader, *os.File, error) {
     return reader, input_file, nil
 }
 
-func genWriter(filepath string) (*bufio.Writer, *os.File, error) {
+// genWriter helper function acquires a bufio.Writer instance
+// pointer to a write-mode openend file.
+// You can use it to, for example, to write lines into a file.
+// It takes in parameter the path to the file you'd wanna
+// acquire a Reader for, and returns a bufio.Reader instance pointer,
+// a pointer to the opened os.File descriptor and error.
+// Plus, it opens the file in O_CREATE and O_WRONLY mode,
+// With a 8192 bytes buffer.
+func GenWriter(filepath string) (*bufio.Writer, *os.File, error) {
     output_file, err := os.OpenFile(filepath, os.O_CREATE | os.O_WRONLY, 0777)
     if err != nil { return nil, nil, err }
 
     writer := bufio.NewWriterSize(output_file, 8192)
 
     return writer, output_file, nil
+}
+
+// GenPartFilename helper function generates a part file
+// name, adding extension, and number to the base file name
+func GenPartFilename(basename string, ext string, num int) string{
+    full_extension := "." + ext + strconv.Itoa(num)
+    filename := basename + full_extension
+
+    return filename
 }
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+  "path"
+  "bufio"
+  "fmt"
   "log"
   "flag"
   "io"
+  "os"
 )
 
 // /*
@@ -11,13 +15,13 @@ import (
 // */
 var input string
 var output string
-var sample string
+var sample int
 
 func init() {
   const (
-    defaultInput = "Input file"
-    defaultOutput = "Ouput directory"
-    defaultSample = "Line count per output file"
+    defaultInput = ""
+    defaultOutput = ""
+    defaultSample = 25
     usage = "butcher -input bigfatfile -output dir -sample linecountpersubfile"
   )
 
@@ -27,8 +31,8 @@ func init() {
   flag.StringVar(&output, "output", defaultOutput, usage)
   flag.StringVar(&output, "o", defaultOutput, usage + " (shorthand)")
 
-  flag.StringVar(&sample, "sample", defaultSample, usage)
-  flag.StringVar(&sample, "s", defaultSample, usage + " (shorthand)")
+  flag.IntVar(&sample, "sample", defaultSample, usage)
+  flag.IntVar(&sample, "s", defaultSample, usage + " (shorthand)")
 }
 
 func check(e error) {
@@ -38,15 +42,39 @@ func check(e error) {
 }
 
 func main() {
+    var writer                              *bufio.Writer
+    var output_file                       *os.File
+    var linenum, filenum               int = 0, 1
+
+    // Parse command line arguments
     flag.Parse()
 
-    reader, input_file, err := genReader(input)
-    writer, output_file, err := genWriter(output)
+    // Acquire a Reader over input file
+    reader, input_file, err := GenReader(input)
     defer input_file.Close()
-    defer output_file.Close()
     if err != nil {log.Fatal(err)}
 
+    // If output dir does not exists yet, create it.
+    if  val, err := exists(output) ; val == false {
+      err = os.MkdirAll(output, 0777)
+      if err != nil { log.Fatal(err) }
+    }
+
     for {
+        if (linenum % sample) == 0 {
+            // Generate part file path
+            partfile_name := GenPartFilename(input, "part", filenum)
+            outfile_path := path.Join(output, partfile_name)
+            fmt.Println(outfile_path)
+
+            // Acquire a bufio.Writer pointing to part file
+            writer, output_file, err = GenWriter(outfile_path)
+            defer output_file.Close()
+            if err != nil { log.Fatal(err) }
+
+            filenum++
+        }
+
         line, _, err := reader.ReadLine()
         if err == io.EOF { break }
 
@@ -54,5 +82,6 @@ func main() {
         if err != nil { log.Fatal(err) }
 
         writer.Flush()
+        linenum++
     }
 }
